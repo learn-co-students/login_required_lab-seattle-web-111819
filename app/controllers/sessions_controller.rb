dadbe77cf6f9994bb526ead684fc2143a47260df
NicoleLC16 <asalwaysnicole@outlook.com>
2019-12-29 11:48:53 -0800

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+    def new
+
+    end
+
+    def create
+        if !params[:name] || params[:name].empty?
+            require_login
+        else
+            session[:name] = params[:name]
+            require_login
+        end
+    end
+
+    def destroy
+        session.delete :name
+    end
+end
